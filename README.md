53c1fb190063593b4558fa81da3de533c04ea0a7
Matei Nicolae <nmatei@sdl.com>
2022-03-18 20:48:19 +0200

    create-repos-list.sh - Backup your git repos

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# scripts
-Custom Scripts
+# Scripts
+Custom Scripts for your workstation!
+
+## create-repos-list.sh - Backup your git repos structure
+
+Usefull when move to a new device.
+The script create same folder structure with all git repos you already have on old machine.
+
+- [x] edit [create-repos-list.sh](create-repos-list.sh) and change out=/c/Products/restore-repos-list.sh
+- [x] edit [create-repos-list.sh](create-repos-list.sh) and declare -a folders that you want to backup
+- [x] On old device run [create-repos-list.sh](create-repos-list.sh)
+- [x] Then and copy and run **restore-repos-list.sh** on new machine
+
+```sh
+./
+# or
+./restore-repos-list.sh
+```
+
---
+++ create-repos-list.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+# 
+# Running:
+#
+#   ./create-repos-list.sh
+#   ./restore-repos-list.sh
+# 
+# Notes:
+#  - make sure you have setted git sh keys to be able to clone
+#
+# ==========================================
+
+# TODO create in current folder or get as param
+out=/c/Products/restore-repos-list.sh
+
+# no commas in array
+declare -a folders=(
+	"/c/Products"
+	#"/c/Products/extra-packages"
+)
+
+# ==========================================
+# ==========================================
+# ==========================================
+
+echo create $out
+touch $out
+
+> $out
+
+parseFolders()
+{
+	dir=$1
+	echo "list git apps in: $dir"
+	
+	echo mkdir "$dir" >> $out
+	echo cd $dir >> $out
+	cd $dir
+
+	for app in *; do
+	  echo $app
+	  echo "" >> $out
+	  if [ -d "$dir/$app/" ]
+	  then
+		cd ./$app
+		path=$(git config --get remote.origin.url)
+		cd ..
+		
+		if [ -n "$path" ]
+		then
+			echo "    $path"
+			echo "# $app" >> $out
+			echo "git clone $path $app" >> $out
+		else
+			echo "# $app - not a git repo" >> $out
+			echo mkdir "$app" >> $out
+			# parseFolders "$dir/$app"
+		fi
+	  else
+		echo "# $app" >> $out
+		echo "$app - is file"
+		echo "echo $app - file!" >> $out
+	  fi
+	  echo "echo \"\"" >> $out
+	done
+}
+
+# iterate in all folders
+
+arraylength=${#folders[@]}
+
+for (( i=0; i<${arraylength}; i++ ));
+do
+  echo ""
+  echo "" >> $out
+  echo "  $i) parse folder: ${folders[$i]}"
+  parseFolders ${folders[$i]}
+  echo ""
+  echo "" >> $out
+  echo "echo \"\"" >> $out
+done
