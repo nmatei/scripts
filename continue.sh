b64253ecbde9b3f9e32058c08f6283a5d7dc4f03
Matei Nicolae <nmatei@sdl.com>
2022-04-01 16:42:00 +0300

    remove continue file

--- continue.sh
+++
@@ -1,9 +0,0 @@
-echo This script will use git clone some projects.
-goon=
-read -p "Do you want to continue? (y/n): " goon 
-if [[ $goon != 'y' ]]
-then
-    echo "Operation Canceled!!"
-    exit
-fi
-echo "Start cloning Projects..."
