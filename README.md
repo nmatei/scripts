6202d4ffb80ceb1d8d3205bccf59320248eed360
Matei Nicolae <nmatei@sdl.com>
2022-04-01 16:41:42 +0300

    add colors when create repos list. add question y/n before restore

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Scripts
+
 Custom Scripts for your workstation!
 
 ## create-repos-list.sh - Backup your git repos structure
@@ -7,21 +8,27 @@ Usefull when move to a new device and have to clone many git projects.
 The script will re create same folder structures with all git repos you already have on old machine in specified folders.
 
 ### Running:
+
 - [x] Download/clone and edit [create-repos-list.sh](create-repos-list.sh) and update main **folders** where you have cloned your apps
-    ```sh
-    declare -a folders=(
-        "/c/Products"
-        "/c/Products/extra-packages"
-    )
-    ```
-- [x] Run [create-repos-list.sh](create-repos-list.sh) on your old device (or colleagues):
-    ```sh
-    ./create-repos-list.sh
-    ```
+
+```sh
+declare -a folders=(
+    "/c/Products"
+    "/c/Products/extra-packages"
+)
+```
+
+- [x] Run [create-repos-list.sh](create-repos-list.sh) on your old device (or colleagues), using **git bash** terminal:
+
+```sh
+./create-repos-list.sh
+```
+
 - [x] Then and copy and run **restore-repos-list.sh** on new machine:
-    ```sh
-    ./restore-repos-list.sh
-    ```
+
+```sh
+./restore-repos-list.sh
+```
 
 ### create-repos-list.sh - results
 
@@ -34,4 +41,3 @@ git clone git@github.com:nmatei/node-api.git node-api
 git clone git@github.com:nmatei/scripts.git scripts
 # ...
 ```
-
--- create-repos-list.sh
+++ create-repos-list.sh
@@ -20,6 +20,13 @@ declare -a folders=(
 # ==========================================
 # Output
 # ==========================================
+# https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
+Red='\033[0;31m'
+BGreen='\033[1;32m'
+BYellow='\033[1;33m'
+Cyan='\033[0;36m'
+NC='\033[0m' # No Color
+# == == == == == == == ==
 
 BASEDIR="$( cd "$( dirname "$0" )" && pwd )"
 echo "Script location: ${BASEDIR}"
@@ -34,19 +41,38 @@ out=$BASEDIR/restore-repos-list.sh
 echo create $out
 touch $out
 
+# empty $out file
 > $out
+
+# ==========================================
+# Continue Question (y/n):
+# ==========================================
+echo "
+# ==========================================
+# = = =       Ask if Continue?         = = =
+# ==========================================
+echo This script will use git clone some projects.
+goon=
+read -p \"Do you want to continue? (y/n): \" goon 
+if [[ \$goon != 'y' ]] 
+then
+    echo Operation Canceled!
+    exit
+fi
+echo Start cloning your Projects...
+# ==========================================
+" >> $out
+# ==========================================
 
 parseFolders()
 {
 	dir=$1
-	echo "list git apps in: $dir"
-	
 	echo mkdir "$dir" >> $out
 	echo cd $dir >> $out
 	cd $dir
 
 	for app in *; do
-	  echo $app
+	  echo -e "${BYellow}$app${NC}"
 	  echo "" >> $out
 	  if [ -d "$dir/$app/" ]
 	  then
@@ -56,17 +82,18 @@ parseFolders()
 		
 		if [ -n "$path" ]
 		then
-			echo "    $path"
+			echo -e "    ${Cyan}$path${NC}"
 			echo "# $app" >> $out
 			echo "git clone $path $app" >> $out
 		else
-			echo "# $app - not a git repo" >> $out
+			echo -e "    ${Red}not a git folder!${NC}"
+			echo "# $app - not a git folder" >> $out
 			echo mkdir "$app" >> $out
 			# parseFolders "$dir/$app"
 		fi
 	  else
 		echo "# $app" >> $out
-		echo "$app - is file"
+		echo -e "    ${Red}is a file!${NC}"
 		echo "echo $app - file!" >> $out
 	  fi
 	  echo "echo \"\"" >> $out
@@ -81,7 +108,7 @@ for (( i=0; i<${arraylength}; i++ ));
 do
   echo ""
   echo "" >> $out
-  echo "  $i) parse folder: ${folders[$i]}"
+  echo -e "${BGreen}$i) Parse folder: ${folders[$i]} ${NC}"
   parseFolders ${folders[$i]}
   echo ""
   echo "" >> $out
