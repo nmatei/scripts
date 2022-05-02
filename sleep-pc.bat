3f8da3fb861996b94477d7e24907218c481b990b
Matei Nicolae <nmatei@sdl.com>
2022-05-02 22:32:48 +0300

    add shortcut to 5 sec using y key

--- sleep-pc.bat
+++ sleep-pc.bat
@@ -3,9 +3,12 @@ echo ============================================
 echo =========== System Sleep     ===============
 echo ============================================
 
-echo Specify Time (minutes):
+echo Specify Time ([number] = minutes / [y] = 5sec]):
 SET /p SEC=
 IF NOT DEFINED SEC SET "SEC=0"
+
+if /i ""%SEC%"" == ""y"" SET "SEC=5" && goto exec
+
 SET /A SEC *= 60
 
 if /i not ""%SEC%"" == ""0"" goto timerStart
@@ -31,7 +34,7 @@ rem "%ProgramFiles%\Winamp\CLAmp.exe" /STOPFADE
 
 echo Sleeping...
 
-rundll32.exe powrprof.dll,SetSuspendState 0,1,0
+rem rundll32.exe powrprof.dll,SetSuspendState 0,1,0
 
 goto end
 
